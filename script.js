e5780600809625dc2158ee1bdbb96e71fc453bad
Imran Warfa <112869298+imranwarfa@users.noreply.github.com>
2024-03-13 22:06:54 -0400

    Borders to individual divs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const rows = 16;
-const columns = 16;
+const DEFAULT_SIZE = 16;
+const gridContainer = document.getElementById('myGrid')
 
 // generate a randomColor using Math.random
 function getRandomColor() {
@@ -9,28 +9,47 @@ function getRandomColor() {
     return `rgb(${r},${g},${b})`; // Construct RGB color string
 }
 
-// get the grid container element
-const gridContainer = document.getElementById('myGrid');
+// generate grid based on size provided; Note Size => size(row) x size(column)
+function createGrid(size){
+    rows = size;
+    columns = size;
 
-// Create the grid dynamically
-for(let i = 0; i < rows; i++){
-    for (let j = 0; j < columns; j++){
-        // Create a grid element
-        const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
-        //gridItem.textContent = "Grid item row: " + i + " column: " + j;
+    // Create the grid dynamically
+    for(let i = 0; i < rows; i++){
+        for (let j = 0; j < columns; j++){
+            // Create a grid element
+            const gridItem = document.createElement('div');
+            gridItem.className = 'grid-item';
+            //gridItem.textContent = "Grid item row: " + i + " column: " + j;
 
-        gridItem.style.backgroundColor = getRandomColor();
-
-        // add an eventListener to each gridItem to watch for when the mouse hovers over the div
-        // change color when this happens
-
-        gridItem.addEventListener('mouseover', event => {
             gridItem.style.backgroundColor = getRandomColor();
-        });
-        
-        // Append gridItem
-        gridContainer.appendChild(gridItem);
+            gridItem.style.border = '2px solid black';
+
+            // add an eventListener to each gridItem to watch for when the mouse hovers over the div
+            // change color when this happens
+
+            gridItem.addEventListener('mouseover', event => {
+                gridItem.style.backgroundColor = getRandomColor();
+            });
+            
+            // Append gridItem
+            gridContainer.appendChild(gridItem);
+        }
     }
 }
+
+// create initial grid of 16 x 16;
+createGrid(DEFAULT_SIZE);
+
+
+
+// create button to change number of grid elements by row & column
+
+
+
+// Implement a black drawing method instead of color ---- Need borders here most likely
+
+
+
+// Implement an eraser --- just make background white again, need borders here too
 
